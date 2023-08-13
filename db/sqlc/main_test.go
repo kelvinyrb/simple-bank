7cca3e2185cef7b9ce526c2f5442762299c646c4
kelvinyrb <kelvin.ye@zetifi.com>
2023-08-13 10:59:50 +1000

    Start unit testing.

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,3 +1,29 @@
 package db
 
-var TestQueries *Queries
+// Tell the Go formatter to keep the github import with _ prefix
+// because we're not calling any functions from that package.
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kelvinyrb/simple-bank
 
 go 1.21.0
+
+require github.com/lib/pq v1.7.0
