ffeedf0cbbaa8fd08682d1b6662d06e273a43c34
kelvinyrb <kelvin.ye@zetifi.com>
2023-09-21 22:31:09 +1000

    S2L2 complete. Env variables.

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -8,21 +8,26 @@ import (
 	"os"
 	"testing"
 
+	"github.com/kelvinyrb/simple-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
+// const (
+// 	dbDriver = "postgres"
+// 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+// )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	// var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
 	// Don't use := synx here because we already declared the variables
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 
 	"github.com/kelvinyrb/simple-bank/api"
 	db "github.com/kelvinyrb/simple-bank/db/sqlc"
+	"github.com/kelvinyrb/simple-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
 	}
