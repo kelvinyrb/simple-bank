c4434aca600ace9f451d3a3bbb03680d42956729
kelvinyrb <kelvin.ye@zetifi.com>
2023-10-15 17:12:35 +1100

    L23 complete. Login User API Paseto/JWT tokens.

--- api/server.go
+++ api/server.go
@@ -1,30 +1,55 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/kelvinyrb/simple-bank/db/sqlc"
+	"github.com/kelvinyrb/simple-bank/token"
+	"github.com/kelvinyrb/simple-bank/util"
 )
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
 	// store  *db.Store commented out because store is no longer a struct pointer but an interface
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
 	// router.SetTrustedProxies(nil)
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+
+	// HW: update and delete account endpoints
+
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -32,10 +57,7 @@ func NewServer(store db.Store) *Server {
 
 	router.POST("/transfers", server.createTransfer)
 
-	// HW: update and delete account endpoints
-
 	server.router = router
-	return server
 }
 
 // Start runs the HTTP server on a specific address.
