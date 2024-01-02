99e2f205e36bb00a085be196d8816bc433b6a19a
kelvinyrb <kelvin.ye@zetifi.com>
2024-01-02 14:35:47 +1100

    Add authorisation to protect update user api.

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -17,9 +17,10 @@ import (
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
 	// authPayload, err := server.authorizeUser(ctx, []string{util.BankerRole, util.DepositorRole})
-	// if err != nil {
-	// 	return nil, unauthenticatedError(err)
-	// }
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
 
 	violations := validateUpdateUserRequest(req)
 	if violations != nil {
@@ -29,6 +30,10 @@ func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest)
 	// if authPayload.Role != util.BankerRole && authPayload.Username != req.GetUsername() {
 	// 	return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	// }
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
+	}
 
 	arg := db.UpdateUserParams{
 		Username: req.GetUsername(),
