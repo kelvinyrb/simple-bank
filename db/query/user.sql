c9fc960ecf1c9720efb60e97e57ee98e19d1ca22
kelvinyrb <kelvin.ye@zetifi.com>
2024-01-02 12:55:57 +1100

    Implement update user rpc.

--- db/query/user.sql
+++ db/query/user.sql
@@ -15,6 +15,10 @@ LIMIT 1;
 -- name: UpdateUser :one
 UPDATE users
 SET hashed_password = COALESCE(sqlc.narg(hashed_password), hashed_password),
+    password_changed_at = COALESCE(
+        sqlc.narg(password_changed_at),
+        password_changed_at
+    ),
     full_name = COALESCE(sqlc.narg(full_name), full_name),
     email = COALESCE(sqlc.narg(email), email)
 WHERE username = sqlc.arg(username)
