e091d8455b9ddc896cffb5392c77da27284abf75
kelvinyrb <kelvin.ye@zetifi.com>
2024-01-13 17:47:01 +1100

    Handle errors and print pretty logs for async workers.

--- worker/processor.go
+++ worker/processor.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hibiken/asynq"
 	db "github.com/kelvinyrb/simple-bank/db/sqlc"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -25,7 +26,7 @@ type RedisTaskProcessor struct {
 
 // func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store, mailer mail.EmailSender) TaskProcessor {
 func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
-	// logger := NewLogger()
+	logger := NewLogger()
 	// redis.SetLogger(logger)
 
 	server := asynq.NewServer(
@@ -35,11 +36,11 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
-			// ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
-			// 	log.Error().Err(err).Str("type", task.Type()).
-			// 		Bytes("payload", task.Payload()).Msg("process task failed")
-			// }),
-			// Logger: logger,
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).
+					Bytes("payload", task.Payload()).Msg("process task failed")
+			}),
+			Logger: logger,
 		},
 		// asynq.Config{},
 	)
